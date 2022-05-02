ab691a69dd1c8339d18042a8dcedfb183eb25819
Ardakaniz <ardakaniz@e.email>
2022-05-02 19:23:47 +0200

    Fix MCMC flipping in sweep

--- src/mcmc.rs
+++ src/mcmc.rs
@@ -1,11 +1,12 @@
 use crate::lattice::{Lattice, LATTICE_SIZE};
 
-use rand::{Rng, rngs::ThreadRng};
+use rand::{Rng, rngs::ThreadRng, distributions::{Distribution, Uniform}};
 
 pub struct MCMC<'a> {
 	lattice: &'a mut Lattice,
 	temperature: f64,
 	rng: ThreadRng,
+	spin_id_distrib: Uniform<usize>,
 }
 
 impl<'a> MCMC<'a> {
@@ -14,11 +15,15 @@ impl<'a> MCMC<'a> {
 			lattice,
 			temperature,
 			rng: rand::thread_rng(),
+			spin_id_distrib: Uniform::new(0, LATTICE_SIZE*LATTICE_SIZE)
 		}
 	}
 
 	pub fn sweep(&mut self) {
 		for _ in 0..LATTICE_SIZE*LATTICE_SIZE {
+			let i = self.spin_id_distrib.sample(&mut self.rng);
+			let j = self.spin_id_distrib.sample(&mut self.rng);
+			
 			let (i, j) = self.lattice.flip_random_spin();
 			let energy_diff = self.lattice.energy_diff(i, j);
 
